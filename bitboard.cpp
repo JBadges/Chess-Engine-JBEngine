5e3c989c776d54d84c045c50eb1992596218fcda
BadgeSWS <jackson.brajer@gmail.com>
2021-05-17 23:05:17 -0400

    Bitboard

---
+++ bitboard.cpp
@@ -0,0 +1,22 @@
+#include "bitboard.h"
+#include <iostream>
+
+void print_bitboard(const Bitboard bb)
+{
+    printf("\n");
+    for (int rank = 0; rank < 8; rank++)
+    {
+        for (int file = 0; file < 8; file++)
+        {
+            const int square = rank * 8 + file;
+
+            if (!file)
+                printf(" %d ", 8 - rank);
+
+            printf(" %d ", get_bit(bb, square) ? 1 : 0);
+        }
+        printf("\n");
+    }
+    printf("    a  b  c  d  e  f  g  h\n\n");
+    printf("Bitboard: %llu \n", bb);
+}
--- main.cpp
+++ main.cpp
@@ -1,7 +1,24 @@
 #include <cassert>
 
+#include "bitboard.h"
+
 int main(void)
 {
-	assert(false);
-	return 1;
+	Bitboard b = 0ull;
+	set_bit(b, a2);
+	set_bit(b, b2);
+	set_bit(b, c2);
+	set_bit(b, d2);
+	set_bit(b, e2);
+	set_bit(b, f2);
+	set_bit(b, g2);
+	set_bit(b, h2);
+
+	print_bitboard(b);
+
+	b = shift<DOWN_RIGHT>(b);
+
+	print_bitboard(b);
+
+	return 0;
 }
